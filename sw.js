a6aee3d008631abf3bc194a0318a67dbe5075368
Musaed Alqanaie <mesadhq@gmail.com>
2026-06-15 13:18:55 +0300

    Add per-stop photos, Share button, optional Gov Building

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'japan-trip-v2';
+const CACHE = 'japan-trip-v3';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
